6e82027518bc16c15a496983dc9af107fdbb72e8
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-04-19 14:48:31 +0200

    Added JsonDatabase

---
+++ src/database/jsonDatabase.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const fs = require('node:fs');
+
+const DB_FILENAME = 'database.json';
+const defaultGuildConfig = {
+  leaderRoles: null,
+};
+
+const getCurrentContent = () => {
+  const exists = fs.existsSync(DB_FILENAME);
+  console.log(exists);
+  if (exists) return JSON.parse(fs.readFileSync(DB_FILENAME, 'utf-8'));
+  fs.writeFileSync(DB_FILENAME, '{}');
+  return {};
+};
+
+const updateConfig = (newConfig) => fs.writeFileSync(DB_FILENAME, JSON.stringify(newConfig));
+
+const getGuild = (guildId) => {
+  const config = getCurrentContent();
+  console.log(config);
+  if (!config[guildId]) {
+    config[guildId] = defaultGuildConfig;
+    updateConfig(config);
+  }
+  return config[guildId];
+};
+
+const updateGuild = (guildId, newData) => {
+  const config = getCurrentContent();
+  config[guildId] = newData;
+  updateConfig(config);
+  return true;
+};
+
+const getLeaderRoles = (guildId) => {
+  const guildConfig = getGuild(guildId);
+  return guildConfig.leaderRoles;
+};
+
+module.exports = { getCurrentContent, updateConfig, getGuild, updateGuild, getLeaderRoles };
