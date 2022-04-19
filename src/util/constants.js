5413eec641a5628f280c52c9bf70343bff056c58
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-04-19 14:49:10 +0200

    Created constants (colors, organizations)

---
+++ src/util/constants.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const colors = {
+  red: '#ff0000',
+};
+
+const Organization = {
+  LSPD: 1,
+  LSMC: 2,
+};
+
+const OrganizationNames = {
+  1: 'Полиция г. Лос-Сантос',
+  2: 'Больница г. Лос-Сантос',
+};
+
+module.exports = { colors, Organization, OrganizationNames };
